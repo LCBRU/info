9b97892f5c69b2b398aaa8c68cc1bf9851012177
Richard Bramley <rabramley@gmail.com>
2019-02-02 16:17:22 +0000

    Get Automad working

--- install.py
+++ install.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python3
 
-print("Hello, World")
+import wget
+import shutil
+import zipfile
+import tempfile
+import os
+import subprocess
+from pathlib import Path
+
+
+HTTP_DIR='/local/www/htdocs/'
+
+
+def get_automad():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        os.chdir(tmpdirname)
+
+        filename = wget.download('https://automad.org/download')
+
+        with zipfile.ZipFile(filename,"r") as zip_ref:
+            zip_ref.extractall(tmpdirname)
+
+        extracted_dir = next(x for x in Path(tmpdirname).iterdir() if x.is_dir())
+
+        shutil.rmtree(HTTP_DIR)
+        shutil.copytree(extracted_dir.name, HTTP_DIR)
+
+
+get_automad()
+
+subprocess.run(['setfacl', '-m', 'u:wwwrun:rwx', '{}/cache'.format(HTTP_DIR)])
