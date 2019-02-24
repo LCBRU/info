64739d89b71bdf88210e38dc4aefa83abf4c9430
Richard Bramley <rabramley@gmail.com>
2019-02-24 19:27:44 +0000

    Write access to pages

--- install.py
+++ install.py
@@ -36,3 +36,4 @@ def get_automad():
 get_automad()
 
 subprocess.run(['setfacl', '-m', 'u:wwwrun:rwx', '{}/cache'.format(HTTP_DIR)])
+subprocess.run(['setfacl', '-m', 'u:wwwrun:rwx', '{}/pages'.format(HTTP_DIR)])
