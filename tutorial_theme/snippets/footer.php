ea8e7c1ba0994fd5175dbe9337b2ee90e2f2dd6f
Richard Bramley <rabramley@gmail.com>
2019-03-26 10:35:24 +0000

    BRC theme

--- tutorial_theme/snippets/footer.php
+++ tutorial_theme/snippets/footer.php
@@ -1,37 +1,16 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
-		<section id="source" class="section">
-			<hr />
-			<div class="tags has-addons">
-				<span class="tag is-dark">Source</span>
-				<span class="tag is-light">/packages/@{ theme }/@{ :template }.php</span>
-			</div>
-			<div class="content">
-				<@ tutorial/source { 
-					file: '/packages/@{ theme }/@{ :template }.php' 
-				} @>
-			</div>
-		</section>
-		<section class="section">	
-			<nav class="breadcrumb" aria-label="breadcrumbs">
-				<ul>
-					<@ newPagelist { 
-						type: 'breadcrumbs',
-						excludeHidden: false
-					} @>
-					<@ foreach in pagelist @>
-						<li><a href="@{ url }">@{ title }</a></li>
-					<@ end @>
-				</ul>
-			</nav>
-		</section>
-		<section class="section">
-			Made with <a href="https://automad.org">Automad</a>
-			<br />
-			Released under the <a href="https://automad.org/license">MIT license</a>
-			<br />
-			&copy; @{ :now | dateFormat('Y') } by
-			<a href="https://marcdahmen.de">Marc Anton Dahmen</a>
-		</section>
+</div>
+
+<footer>
+	<div class="container">
+		Copyright &copy; 2018 NIHR Leicester Biomedical Research Centre
 	</div>
+</footer>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 </body>
 </html>
