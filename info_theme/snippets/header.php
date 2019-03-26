2b0255a6883c9a3324273b5b9f8651cdf55fff2e
Richard Bramley <rabramley@gmail.com>
2019-03-26 16:03:47 +0000

    Styling for not uhl links

--- info_theme/snippets/header.php
+++ info_theme/snippets/header.php
@@ -18,7 +18,7 @@ if ($_SERVER['HTTP_X_FORWARDED_FOR'] == '86.17.1.198') {
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-    <link rel="stylesheet" href="/packages/lbrc/info_theme/css/brc.css?v=4">
+    <link rel="stylesheet" href="/packages/lbrc/info_theme/css/brc.css?v=5">
 
     <title>BRC Intranet</title>
   </head>
