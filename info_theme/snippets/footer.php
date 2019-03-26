06ae58299932aed17f0246327dbc3fb3c4fd26ce
Richard Bramley <rabramley@gmail.com>
2019-03-26 09:36:12 +0000

    Add navbar

--- info_theme/snippets/footer.php
+++ info_theme/snippets/footer.php
@@ -1,27 +1,33 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
-                <section class="section">
-                        <nav class="breadcrumb" aria-label="breadcrumbs">
-                                <ul>
-                                        <@ newPagelist { 
-                                                type: 'breadcrumbs',
-                                                excludeHidden: false
-                                        } @>
+
+                </div> <# .uk-block #>
+                <div class="footer uk-text-muted uk-margin-large-bottom uk-margin-top">
+                        <ul class="uk-grid grid-margin uk-grid-width-medium-1-2">
+                                <li>
+                                        <# Show menu with pages with checked "checkboxShowInFooter". #>
+                                        <@ newPagelist { excludeHidden: false } @>
                                         <@ foreach in pagelist @>
-                                                <li><a href="@{ url }">@{ title }</a></li>
+                                                <@ if @{ checkboxShowInFooter } @>
+                                                        <a href="@{ url }">@{ title }</a>
+                                                        <br />
+                                                <@ end @>
                                         <@ end @>
-                                </ul>
-                        </nav>
-                </section>
-                <section class="section">
-                        Made with <a href="https://automad.org">Automad</a>
-                        <br />
-                        Released under the <a href="https://automad.org/license">MIT license</a>
-                        <br />
-                        &copy; @{ :now | dateFormat('Y') } by
-                        <a href="https://marcdahmen.de">Marc Anton Dahmen</a>
-                </section>
+                                        <a href="/dashboard">Sign in</a>
+                                </li>
+                                <li class="uk-text-right uk-text-left-small">
+                                        <a href="/">
+                                                <i class="uk-icon-copyright"></i>
+                                                @{ :now | dateFormat('Y') } @{ sitename }
+                                        </a>
+                                </li>
+                        </ul>
+
+                        <# Add optional footer items. #>
+                        @{ itemsFooter }
+
+                </div>
+
         </div>
+
 </body>
 </html>
-
-
