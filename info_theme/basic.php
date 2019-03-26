1fffce796b5e2cb38cb5c2fc467467af4ab0cb2d
Richard Bramley <rabramley@gmail.com>
2019-03-26 11:57:46 +0000

    Cards

--- info_theme/basic.php
+++ info_theme/basic.php
@@ -1,17 +1,16 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!');?>
 <@ snippets/header.php @>
 
-	<div class="container">
-		
-		<div class="row">
-			<div id="title" class="col-md-12">
-				<h2>@{ title }</h2>
-			</div>
-			<div class="col-md-6">
-                @{ text }
-			</div>
-		</div>		
-			
-	</div>		
-	
+<div>
+	<section>
+		<h2>@{ title }</h2>	
+	</section>
+
+	<div class="card">
+		<div class="card-body">
+			@{ text }
+		</div>
+	</div>
+div>
+
 <@ snippets/footer.php @>
