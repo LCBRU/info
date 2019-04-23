46b3f24ad67d1759d09c9fe4d9a673f0587419c8
Richard Bramley <rabramley@gmail.com>
2019-04-23 13:39:56 +0100

    need to specify the theme folder name

--- install.py
+++ install.py
@@ -16,7 +16,6 @@ HTTP_DIR=os.path.join(WWW_DIR, 'htdocs')
 def get_automad():
     pwd = os.path.realpath('.')
 
-    print('A')
     with tempfile.TemporaryDirectory() as tmpdirname:
         os.chdir(tmpdirname)
 
@@ -33,7 +32,6 @@ def get_automad():
         shutil.rmtree(HTTP_DIR)
         shutil.copytree(extracted_dir.name, HTTP_DIR)
 
-    print('B')
     shutil.rmtree(os.path.join(HTTP_DIR, 'cache'))
     shutil.rmtree(os.path.join(HTTP_DIR, 'pages'))
     shutil.rmtree(os.path.join(HTTP_DIR, 'config'))
@@ -44,19 +42,21 @@ def get_automad():
     os.symlink(os.path.join(WWW_DIR, 'config'), os.path.join(HTTP_DIR, 'config'))
     os.symlink(os.path.join(WWW_DIR, 'shared'), os.path.join(HTTP_DIR, 'shared'))
 
-    print('C')
     with tempfile.TemporaryDirectory() as tmpdirname:
         os.chdir(tmpdirname)
 
+        print('1')
         theme_zip = wget.download('https://github.com/LCBRU/brc_automad_theme/archive/master.zip')
+        print('2')
 
         with zipfile.ZipFile(theme_zip,"r") as zip_ref:
             zip_ref.extractall(tmpdirname)
 
+        print('3')
         extracted_dir = next(x for x in Path(tmpdirname).iterdir() if x.is_dir())
 
+        print('4')
         shutil.copytree(extracted_dir.name, os.path.join(HTTP_DIR, 'packages/brc_automad_theme'))
-    print('D')
 
 
 get_automad()
