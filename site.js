54c195e454629fd6fe2792f7841dbc8099acf2f7
Richard Bramley <rabramley@gmail.com>
2019-04-23 14:54:29 +0100

    Not the correct location

--- site.js
+++ site.js
@@ -1,4 +1,4 @@
-$.get( "/packages/lbrc/brc_automad_theme/ip.php", function( data ) {
+$.get( "/packages/brc_automad_theme/ip.php", function( data ) {
     if (data != 'uhl') {
         $("body").addClass("not_uhl");
     } else if (data != 'uol') {
