d96f8cbe2901e917f9ea589588f4563f67ca074a
Richard Bramley <rabramley@gmail.com>
2020-12-02 15:05:55 +0000

    Lookup protocol statements

--- site.js
+++ site.js
@@ -11,9 +11,11 @@ $(document).ready(function(){
     $('.protocol_statement').each(function() {
         ps = $(this);
 
-        $.ajax({
-            url: "https://info.lbrc.le.ac.uk/infomation_governance/text/" +  ps.data("protocol-statement-id"),
-        }).always(function(data) {
+        url = "https://info.lbrc.le.ac.uk/infomation_governance/text/" +  ps.data("protocol-statement-id");
+
+        alert(url);
+
+        $.get(url, function(data) {
             alert(data);
             ps.html(data);
         });
