e70191edc2753b5952b62972eb44d6c69d3803eb
Richard Bramley <rabramley@gmail.com>
2020-12-04 13:23:05 +0000

    Try a closure

--- site.js
+++ site.js
@@ -5,23 +5,3 @@ $.get( "/packages/brc_automad_theme/ip.php", function( data ) {
         $("body").addClass("not_uol");
     }
 });
-
-
-$(document).ready(function(){
-    $('.protocol_statement').each(function() {
-        ps = $(this);
-
-        url = "https://info.lbrc.le.ac.uk/information_governance/security_statement/text/" +  ps.data("protocol-statement-id");
-
-        callback = function(psc) {
-            return function (data) {
-                content = '<p>' + data.replace(/\n([ \t]*\n)+/g, '</p><p>').replace('/\n/g', '<br />') + '</p>';
-                psc.html(content);
-            }
-        }
-        c = callback(ps);
-
-        $.get(url, c);
-
-    });
-  });
